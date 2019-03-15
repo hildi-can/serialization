eb75ac7164e1389c3dffe3718db204d645631b94
solo-yolo <sergei.sologub@gmail.com>
2019-03-15 16:25:09 +0200

    add serialization interface & data models

---
+++ src/main/java/io/github/hildi/can/model/User.java
@@ -0,0 +1,69 @@
+package io.github.hildi.can.model;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+
+public class User {
+
+    private final Long id;
+    private final String nickName;
+    private String email;
+    private FullName fullName;
+    private Collection<String> permissions;
+    private Map<String, String> attributes;
+    private Date createdAt;
+
+    public User(Long id, String nickName) {
+        this.id = id;
+        this.nickName = nickName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public FullName getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
+
+    public Collection<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Collection<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
