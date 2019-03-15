e31a62ecf638c9ca589e534563bba8697ea14024
Serhii Solohub <sergei.sologub@gmail.com>
2019-03-15 17:23:50 +0200

    add serialization interface & data models (#1)
    add serialization interface & data models
    remove sonar analysis at all

---
+++ src/main/java/io/github/hildi/can/service/SerializationService.java
@@ -0,0 +1,27 @@
+package io.github.hildi.can.service;
+
+import io.github.hildi.can.model.User;
+
+import java.io.File;
+import java.net.URI;
+
+public interface SerializationService {
+
+    /**
+     * Reads data from the file, specified by file argument and constructs instance of {@link User} class from it.
+     * Details depends on the implementation.
+     *
+     * @param file - the file, to read user data from
+     * @return instance of {@link User} class, never null
+     */
+    User deserialize(File file);
+
+    /**
+     * Stores {@link User} instance's data to the file, described by the file argument.
+     *
+     * @param user - user to store
+     * @param uri - file, where data must be stored
+     */
+    void serialize(User user, URI uri);
+
+}
