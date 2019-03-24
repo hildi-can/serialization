46291047793a58e56a66eca031be6b5c2557e944
sergeygildi <sergeygildi@gmail.com>
2019-03-25 00:12:25 +0200

    Update tests

--- src/test/java/io/github/hildi/can/service/StandardJavaSerializationServiceTest.java
+++ src/test/java/io/github/hildi/can/service/StandardJavaSerializationServiceTest.java
@@ -1,14 +1,19 @@
 package io.github.hildi.can.service;
 
+import io.github.hildi.can.exceptions.DeserializeException;
+import io.github.hildi.can.exceptions.FileIsNotExistsException;
+import io.github.hildi.can.exceptions.NotWritableFileException;
 import io.github.hildi.can.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Serhii Hildi on 15.03.19.
@@ -36,10 +41,31 @@ class StandardJavaSerializationServiceTest {
         assertEquals(expected, deserialize);
     }
 
-//    @Test
-//    void shouldThrowCannotReadFileException() {
-//        assertThrows(DeserializeException.class, () -> {
-//            service.deserialize(file);
-//        });
-//    }
+    @Test
+    void shouldThrowDeserializeException() {
+        assertThrows(DeserializeException.class, () -> {
+            service.deserialize(file);
+        });
+    }
+
+    @Test
+    void shouldThrowFileIsNotExistsExceptionIfFileIsNotExists() {
+        File mock = mock(File.class);
+        Mockito.when(mock.exists()).thenReturn(false);
+
+        assertThrows(FileIsNotExistsException.class, () -> {
+            service.serialize(user, mock);
+        });
+    }
+
+    @Test
+    void shouldThrowNotWritableFileExceptionIfAppIsAllowedToWriteToTheFile() {
+        File mock = mock(File.class);
+        Mockito.when(mock.exists()).thenReturn(true);
+        Mockito.when(mock.canWrite()).thenReturn(false);
+
+        assertThrows(NotWritableFileException.class, () -> {
+            service.serialize(user, mock);
+        });
+    }
 }
