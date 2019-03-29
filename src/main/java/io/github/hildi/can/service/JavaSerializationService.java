927f6d3605d0f00903eabd051b65219a34f53117
sergeygildi <sergeygildi@gmail.com>
2019-03-29 16:54:38 +0200

    Implement deserialize method. Not fixed.

--- src/main/java/io/github/hildi/can/service/JavaSerializationService.java
+++ src/main/java/io/github/hildi/can/service/JavaSerializationService.java
@@ -15,42 +15,94 @@ import static io.github.hildi.can.service.StandardJavaSerializationService.asser
  */
 public class JavaSerializationService implements SerializationService{
 
-    private StandardJavaSerializationService service = new StandardJavaSerializationService();
-    private User user = new User(2L,"test");
+    private User user;
+
     private long userId;
     private String userNickName;
+    private String userEmail;
+    private Collection<String> userPermissions;
+    private Map<String, String> userAttributes;
+    private Date createdAt;
 
     @Override
     public void serialize(User user, File file) {
-        assertFileExtensionIsCorrect(file);
-        service.serialize(user, file);
+        // TODO
+//      assertFileExtensionIsCorrect(file);
+//      StandardJavaSerializationService service = new StandardJavaSerializationService();
+//      service.serialize(user, file);
     }
 
     @Override
     public User deserialize(File file) {
         assertFileNotNull(file);
         assertFileReadable(file);
+        assertFileExtensionIsCorrect(file);
         try (Scanner scanner = new Scanner(file)){
-            while (scanner.hasNextLine()) {
-                setUserProperties(scanner);
-            }
-            setUserIdAndNickName();
+            getAndAssignParametersFromFile(scanner);
+            setParametersForNewUser(userId, userNickName, userEmail, userPermissions, userAttributes, createdAt);
         } catch (FileNotFoundException e) {
             throw new SerializationException("Failed to deserialize data, reason: file " + file.getName() + " is not found. ", e);
         }
-        return null;
+        return user;
     }
 
-    private void setUserProperties(Scanner scanner) {
+    private void getAndAssignParametersFromFile(Scanner scanner) {
+        while (scanner.hasNextLine()) {
+            getUserPropertiesFromFile(scanner);
+        }
+    }
+
+    private void setIdAndNickNameParameters(long userId, String userNickName) {
+        if (userId != 0 && userNickName != null) {
+            user = new User(userId, userNickName);
+        }
+    }
+
+    private void setParametersForNewUser(long userId, String userNickName, String userEmail, Collection<String> userPermissions,
+                                         Map<String, String> userAttributes, Date createdAt) {
+
+        setIdAndNickNameParameters(userId, userNickName);
+        //print it
+        System.out.println();
+        System.out.println("userId = " + user.getId());
+        System.out.println("userNickName = " + user.getNickName());
+        // end print it
+        setUserEmail(userEmail);
+        setUserPermissions(userPermissions);
+        setUserAttributes(userAttributes);
+        setUserCreatedData(createdAt);
+    }
+
+    private void setUserCreatedData(Date createdAt) {
+        user.setCreatedAt(createdAt);
+        System.out.println("createdAt = " + createdAt);
+    }
+
+    private void setUserAttributes(Map<String, String> userAttributes) {
+        user.setAttributes(userAttributes);
+        System.out.println("userAttributes = " + userAttributes);
+    }
+
+    private void setUserPermissions(Collection<String> userPermissions) {
+        user.setPermissions(userPermissions);
+        System.out.println("userPermissions = " + userPermissions);
+    }
+
+    private void setUserEmail(String userEmail) {
+        user.setEmail(userEmail);
+        System.out.println("userEmail = " + userEmail);
+    }
+
+    private void getUserPropertiesFromFile(Scanner scanner) {
         String[] line = scanner.nextLine().split(" = ");
         String param = line[0].toLowerCase().trim();
 
-        setUserId(param, line);
-        setUserNickName(param, line);
-        setUserEmail(param, line);
-        setUserPermissions(param, line);
-        setUserAttributes(param, line);
-        setUserCreatedData(param, line);
+        getUserId(param, line);
+        getUserNickName(param, line);
+        getUserEmail(param, line);
+        getUserPermissions(param, line);
+        getUserAttributes(param, line);
+        getUserCreatedData(param, line);
 
 //        if (param.equals("firstName".toLowerCase().trim())) {
         // TODO
@@ -62,14 +114,49 @@ public class JavaSerializationService implements SerializationService{
 //        }
     }
 
-    private void setUserIdAndNickName() {
-        if (userId != 0 && userNickName != null){
-            user = new User(userId, userNickName);
-            System.out.println(user.getId()+" "+ user.getNickName());
+    private void getUserId(String param, String[] line) {
+        if (param.equals("id".toLowerCase().trim())) {
+            userId = (long) Integer.parseInt(line[1]);
+            System.out.println("id = " + userId);
         }
     }
 
-    private void setUserCreatedData(String param, String[] line) {
+    private void getUserNickName(String param, String[] line) {
+        if (param.equals("nickName".toLowerCase().trim())) {
+            userNickName = line[1];
+            System.out.println("nickname = " + userNickName);
+        }
+    }
+
+    private void getUserEmail(String param, String[] line) {
+        if (param.equals("email".toLowerCase().trim())) {
+            userEmail = line[1].toLowerCase().trim();
+            System.out.println("email = " + userEmail);
+        }
+    }
+
+    private void getUserPermissions(String param, String[] line) {
+        if (param.equals("permissions".toLowerCase().trim())) {
+            String[] permissionsSplit = line[1].trim().split(",");
+            userPermissions = new ArrayList<>(Arrays.asList(permissionsSplit));
+            System.out.println("permissions = " + userPermissions);
+        }
+    }
+
+    private void getUserAttributes(String param, String[] line) {
+        if (param.equals("attributes".toLowerCase().trim())) {
+            Map<String, String> attributes = new HashMap<>();
+            String[] attributesSplit = line[1].trim().split(";");
+            String[] countrySplit = attributesSplit[0].trim().split(":");
+            String[] citySplit = attributesSplit[1].trim().split(":");
+            attributes.put(countrySplit[0], countrySplit[1]);
+            attributes.put(citySplit[0], citySplit[1]);
+            userAttributes = attributes;
+            System.out.println("attributes = " + userAttributes);
+        }
+    }
+
+    private void getUserCreatedData(String param, String[] line) {
         if (param.equals("createdAt".toLowerCase().trim())) {
             Date date = new Date();
             String[] split = line[1].split("T");
@@ -81,51 +168,8 @@ public class JavaSerializationService implements SerializationService{
             date.setHours((int) Long.parseLong(splitTime[0]));
             date.setMinutes((int) Long.parseLong(splitTime[1]));
             date.setSeconds((int) Long.parseLong(splitTime[2]));
-            user.setCreatedAt(date);
-            System.out.println("date");
-        }
-    }
-
-    private void setUserAttributes(String param, String[] line) {
-        if (param.equals("attributes".toLowerCase().trim())) {
-            Map<String, String> attributes = new HashMap<>();
-            String[] attributesSplit = line[1].trim().split(";");
-            String[] countrySplit = attributesSplit[0].trim().split(":");
-            String[] citySplit = attributesSplit[1].trim().split(":");
-            attributes.put(countrySplit[0], countrySplit[1]);
-            attributes.put(citySplit[0], citySplit[1]);
-            user.setAttributes(attributes);
-            System.out.println("attributes");
-        }
-    }
-
-    private void setUserPermissions(String param, String[] line) {
-        if (param.equals("permissions".toLowerCase().trim())) {
-            String[] permissionsSplit = line[1].split(",");
-            ArrayList<String> permissions = new ArrayList<>(Arrays.asList(permissionsSplit));
-            user.setPermissions(permissions);
-            System.out.println("permissions");
-        }
-    }
-
-    private void setUserEmail(String param, String[] line) {
-        if (param.equals("email".toLowerCase().trim())) {
-            user.setEmail(line[1].toLowerCase().trim());
-            System.out.println("email");
-        }
-    }
-
-    private void setUserNickName(String param, String[] line) {
-        if (param.equals("nickName".toLowerCase().trim())) {
-            userNickName = line[1];
-            System.out.println("nickname");
-        }
-    }
-
-    private void setUserId(String param, String[] line) {
-        if (param.equals("id".toLowerCase().trim())) {
-            userId = (long) Integer.parseInt(line[1]);
-            System.out.println("id");
+            createdAt = date;
+            System.out.println("date = " + createdAt);
         }
     }
 
